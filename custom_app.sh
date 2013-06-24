6f65c64452eb0991515ef20b0261323bbeb5977e
zhangweiping <zhangweiping@baidu.com>
2013-06-24 17:01:52 +0800

    CL-49625: use custom_targetfiles.sh, delete slide unlock in Settings, power_profile.xml
    Change-Id: I3e93150e61705868c3fc8e614c4773a1ce1c381e

--- custom_app.sh
+++ custom_app.sh
@@ -8,4 +8,12 @@ if [ "$apkBaseName" = "Phone" ];then
 	find $tempSmaliDir -name "*.xml" | xargs sed -i 's/ %[ )\%]/ /g'
 	find $tempSmaliDir -name "*.xml" | xargs sed -i 's/\(%[0-9]\$[ds]\)%/\1/g'
         find $tempSmaliDir -name "*\.smali" | xargs sed -i 's#invoke-interface\(.*Lcom/android/internal/telephony/IccCard;->\)#invoke-virtual\1#g'
+
+elif [ "$apkBaseName" = "Settings" ];then
+	echo ">>> in custom_app for Settings"
+	if [ -f $tempSmaliDir/res/xml/security_settings_picker.xml ];then
+		echo ">>> delete unlock_set_baidu_slide line in $tempSmaliDir/res/xml/security_settings_picker.xml"
+		sed -i '/unlock_set_baidu_slide/d' $tempSmaliDir/res/xml/security_settings_picker.xml
+	fi
 fi
+

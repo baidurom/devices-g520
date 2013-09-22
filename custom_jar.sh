90012609116ec54eb34bd92ba6e6dc9bee5a37c7
zoujunhua <zoujunhua@baidu.com>
2013-09-22 16:06:23 +0800

    CL-55556: fixed the TrafficMonitor crash with the dsds branch merge.
    Change-Id: Ib5c9ec8d553fb7cf56a0c829c1b2c2a38aeec226

--- custom_jar.sh
+++ custom_jar.sh
@@ -9,5 +9,14 @@ if [ "$jarBaseName" = "android.policy" ];then
 	sed -i -e "/^\.method.*handleKeyguardDoneDrawing()V/,/^\.end method/d" $tempSmaliDir/smali/com/android/internal/policy/impl/KeyguardViewMediator.smali
 	echo ">>> remove method onScreenTurnedOff(I)V in baidu policy"
 	sed -i -e "/^\.method.*onScreenTurnedOff(I)V/,/^\.end method/d" $tempSmaliDir/smali/com/android/internal/policy/impl/KeyguardViewMediator.smali
+        echo ">>> remove method sendTextMessage(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Landroid/app/PendingIntent;Landroid/app/PendingIntent;I)V in baidu policy"
+	sed -i -e "/^\.method.*sendTextMessage(Ljava\/lang\/String;Ljava\/lang\/String;Ljava\/lang\/String;Landroid\/app\/PendingIntent;Landroid\/app\/PendingIntent;I)V/,/^\.end method/d" $tempSmaliDir/smali/android/telephony/BaiduSmsManager.smali
+
+elif [ "$jarBaseName" = "framework-yi" ];then
+        echo ">>> in custom_jar for framework-yi"
+	echo "jarBaseName: $jarBaseName  tempSmaliDir: $tempSmaliDir"
+	echo ">>> remove method sendTextMessage(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Landroid/app/PendingIntent;Landroid/app/PendingIntent;I)V in framework-yi"
+	sed -i -e "/^\.method.*sendTextMessage(Ljava\/lang\/String;Ljava\/lang\/String;Ljava\/lang\/String;Landroid\/app\/PendingIntent;Landroid\/app\/PendingIntent;I)V/,/^\.end method/d" $tempSmaliDir/smali/android/telephony/BaiduSmsManager.smali
+
 fi
 

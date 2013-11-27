48505ddab7047f6ded8cefbf051ce3fab9a0659e
zhangweiping <zhangweiping@baidu.com>
2013-11-27 22:13:12 +0800

    CL-57033: not need to delete funcs in custom shell
    Change-Id: I54736403fbf06bd29bf030be9c2142699ba52e85

--- custom_app.sh
+++ custom_app.sh
@@ -8,11 +8,6 @@ if [ "$apkBaseName" = "Phone" ];then
 	find $tempSmaliDir -name "*.xml" | xargs sed -i 's/ %[ )\%]/ /g'
 	find $tempSmaliDir -name "*.xml" | xargs sed -i 's/\(%[0-9]\$[ds]\)%/\1/g'
 	find $tempSmaliDir -name "*\.smali" | xargs sed -i 's#invoke-interface\(.*Lcom/android/internal/telephony/IccCard;->\)#invoke-virtual\1#g'
-        sed -i -e "/^\.method.*getIccPinPukRetryTimes()I/,/^\.end method/d" $tempSmaliDir/smali/com/android/phone/PhoneInterfaceManager.smali
-        sed -i -e "/^\.method.*getIccPin1RetryCount()I/,/^\.end method/d" $tempSmaliDir/smali/com/android/phone/PhoneInterfaceManager.smali
-        sed -i -e "/^\.method.*startRecording(ILjava\/lang\/String;)Ljava\/lang\/String/,/^\.end method/d" $tempSmaliDir/smali/com/android/phone/Recorder.smali
-        sed -i -e "/^\.method.*getScAddress()Ljava\/lang\/String/,/^\.end method/d" $tempSmaliDir/smali/com/android/phone/PhoneInterfaceManager.smali
-        sed -i -e "/^\.method.*setScAddress(Ljava\/lang\/String;)V/,/^\.end method/d" $tempSmaliDir/smali/com/android/phone/PhoneInterfaceManager.smali
 
 	echo ">>> use vendor Bluetooth framework in Phone"
 	rm -rf $tempSmaliDir/smali/com/android/phone/Bluetooth*
@@ -25,11 +20,7 @@ elif [ "$apkBaseName" = "Settings" ];then
 		echo ">>> delete unlock_set_baidu_slide line in $tempSmaliDir/res/xml/security_settings_picker.xml"
 		sed -i '/unlock_set_baidu_slide/d' $tempSmaliDir/res/xml/security_settings_picker.xml
 	fi
-        find $tempSmaliDir -name "*\.smali" | xargs sed -i 's#invoke-interface\(.*Lcom/android/internal/telephony/IccCard;->\)#invoke-virtual\1#g'
-
-elif [ "$apkBaseName" = "SystemUI" ];then
-       echo ">>> in custom_app for SystemUI"
-       sed -i -e "/^\.method.*doInBackground(\[Lcom\/android\/systemui\/screenshot\/SaveImageInBackgroundData;)Lcom\/android\/systemui\/screenshot\/SaveImageInBackgroundData/,/^\.end method/d" $tempSmaliDir/smali/com/android/systemui/screenshot/SaveImageInBackgroundTask.smali
+	find $tempSmaliDir -name "*\.smali" | xargs sed -i 's#invoke-interface\(.*Lcom/android/internal/telephony/IccCard;->\)#invoke-virtual\1#g'
 
 fi
 
